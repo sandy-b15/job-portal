771eb67555ab64e79ec67bbdc8e5a883201a865a
arjun singh <arjun.singh@gmail.com>
2022-09-20 18:48:09 +0530

    added new files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import JobOpenings from "./components/JobOpenings";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 function App() {
+  const fetchJobDetailsHandler = async () => {
+    let url = "https://lucidatechnologies-team.freshteam.com/api/job_postings";
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer fyLvFPNHSQUtxJX2sPsq7g`,
+          "Content-Type": "application/json ",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+
+      const loadedData = [];
+
+      for (const key in data) {
+        loadedData.push({
+          id: data[key].id,
+          title: data[key].title,
+        });
+      }
+
+      console.log(loadedData);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchJobDetailsHandler();
+  }, []);
+
   return <JobOpenings />;
 }
 
--- src/components/JobOpenings.js
+++ src/components/JobOpenings.js
@@ -4,25 +4,85 @@ import "./JobOpenings.css";
 const JobOpenings = () => {
   return (
     <Fragment>
-      <section>
-        <div className="job-header">
-          <h1>Current Job Openings</h1>
-        </div>
-        <div className="card job-card">
-          <div className="card-body">
-            <h5 className="card-title mb-2">Marketing Manager</h5>
-            <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
-            <h6 className="card-subtitle mb-2">Job Description</h6>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <div className="btn-actions">
-              <button className="btn btn-apply">Apply</button>
+      <div className="wrapper">
+        <section>
+          <div className="job-header">
+            <h1>Current Job Openings</h1>
+          </div>
+          <div className="job_postings">
+            <div className="card job_card">
+              <div className="card-body">
+                <h5 className="card-title mb-2">Marketing Manager</h5>
+                <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
+                <h6 className="card-subtitle mb-2">Job Description</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <div className="btn-actions">
+                  <button className="apply-button">Apply</button>
+                </div>
+              </div>
+            </div>
+            <div className="card job_card">
+              <div className="card-body">
+                <h5 className="card-title mb-2">Marketing Manager</h5>
+                <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
+                <h6 className="card-subtitle mb-2">Job Description</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <div className="btn-actions">
+                  <button className="apply-button">Apply</button>
+                </div>
+              </div>
+            </div>
+            <div className="card job_card">
+              <div className="card-body">
+                <h5 className="card-title mb-2">Marketing Manager</h5>
+                <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
+                <h6 className="card-subtitle mb-2">Job Description</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <div className="btn-actions">
+                  <button className="apply-button">Apply</button>
+                </div>
+              </div>
+            </div>
+            <div className="card job_card">
+              <div className="card-body">
+                <h5 className="card-title mb-2">Marketing Manager</h5>
+                <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
+                <h6 className="card-subtitle mb-2">Job Description</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <div className="btn-actions">
+                  <button className="apply-button">Apply</button>
+                </div>
+              </div>
+            </div>
+            <div className="card job_card">
+              <div className="card-body">
+                <h5 className="card-title mb-2">Marketing Manager</h5>
+                <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
+                <h6 className="card-subtitle mb-2">Job Description</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <div className="btn-actions">
+                  <button className="apply-button">Apply</button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </Fragment>
   );
 };
