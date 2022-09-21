7bc380744005cef8117b302d105478c7400a1acb
arjun singh <arjun.singh@gmail.com>
2022-09-21 18:36:33 +0530

    changes made

---
+++ src/components/Intro.js
@@ -0,0 +1,30 @@
+import React from "react";
+import image from "../images/img.PNG";
+import "./Intro.css";
+
+const Intro = () => {
+  return (
+    <div>
+      <div className="Card">
+        <div className="intro_div">
+          <h1>Work with us</h1>
+          <p>Join Our Team And Help Shape the Future</p>
+          <div className="button-actions">
+            <button className="apply-button">See Open Positions</button>
+          </div>
+        </div>
+
+        <div className="img_div">
+          <img src={image} alt="Group of people"></img>
+        </div>
+      </div>
+      <p className="para">
+        We're looking for people to join the team who are as excited as we are
+        to help build the platform that empowers the future generation of
+        creators to be successful
+      </p>
+    </div>
+  );
+};
+
+export default Intro;
--- src/components/JobOpenings.js
+++ src/components/JobOpenings.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useCallback, useEffect, useState } from "react";
 import JobListing from "./JobListing";
 import "./JobOpenings.css";
+import { api_token } from "../config/config";
 
 const JobOpenings = () => {
   const [addJobs, setAddJobs] = useState([]);
@@ -12,7 +13,7 @@ const JobOpenings = () => {
       const response = await fetch(url, {
         method: "GET",
         headers: {
-          Authorization: `Bearer fyLvFPNHSQUtxJX2sPsq7g`,
+          Authorization: api_token.token,
           "Content-Type": "application/json ",
         },
       });
@@ -22,10 +23,17 @@ const JobOpenings = () => {
 
       const data = await response.json();
 
+      console.log(data[1]);
+
       const loadedJobData = [];
 
       for (const key in data) {
-        console.log(data[key].description);
+        let dom = document.createElement("div");
+        let frag2 = document
+          .createRange()
+          .createContextualFragment(data[key].description);
+        dom.appendChild(frag2);
+        console.log(dom);
         loadedJobData.push({
           id: key,
           title: data[key].title,
@@ -58,67 +66,9 @@ const JobOpenings = () => {
         <section>
           <div className="job-header">
             <h1>Current Job Openings</h1>
+            <main></main>
           </div>
-          <div className="job_postings">
-            {content}
-
-            {/* <div className="card job_card">
-              <div className="card-body">
-                <h5 className="card-title mb-2">Marketing Manager</h5>
-                <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
-                <h6 className="card-subtitle mb-2">Job Description</h6>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <div className="btn-actions">
-                  <button className="apply-button">Apply</button>
-                </div>
-              </div>
-            </div>
-            <div className="card job_card">
-              <div className="card-body">
-                <h5 className="card-title mb-2">Marketing Manager</h5>
-                <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
-                <h6 className="card-subtitle mb-2">Job Description</h6>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <div className="btn-actions">
-                  <button className="apply-button">Apply</button>
-                </div>
-              </div>
-            </div>
-            <div className="card job_card">
-              <div className="card-body">
-                <h5 className="card-title mb-2">Marketing Manager</h5>
-                <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
-                <h6 className="card-subtitle mb-2">Job Description</h6>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <div className="btn-actions">
-                  <button className="apply-button">Apply</button>
-                </div>
-              </div>
-            </div>
-            <div className="card job_card">
-              <div className="card-body">
-                <h5 className="card-title mb-2">Marketing Manager</h5>
-                <h6 className="card-subtitle mb-4 ">Experience: 2-5 years</h6>
-                <h6 className="card-subtitle mb-2">Job Description</h6>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <div className="btn-actions">
-                  <button className="apply-button">Apply</button>
-                </div>
-              </div>
-            </div> */}
-          </div>
+          <div className="job_postings">{content}</div>
         </section>
       </div>
     </Fragment>
