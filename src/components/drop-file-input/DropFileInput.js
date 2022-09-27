4abb4ebd232391a40301e619ae2210c6184f95d0
arjun singh <arjun.singh@gmail.com>
2022-09-27 13:00:20 +0530

    new changes in the UI

---
+++ src/components/drop-file-input/DropFileInput.js
@@ -0,0 +1,89 @@
+import React, { useRef, useState } from "react";
+import PropTypes from "prop-types";
+
+import "./drop-file-input.css";
+
+import { ImageConfig } from "../../config/ImageConfig";
+import uploadImg from "../../assets/file-upload.svg";
+
+import { FaTrashAlt } from "react-icons/fa";
+
+const DropFileInput = (props) => {
+  const wrapperRef = useRef(null);
+
+  const [fileList, setFileList] = useState([]);
+
+  const onDragEnter = () => wrapperRef.current.classList.add("dragover");
+
+  const onDragLeave = () => wrapperRef.current.classList.remove("dragover");
+
+  const onDrop = () => wrapperRef.current.classList.remove("dragover");
+
+  const onFileDrop = (e) => {
+    const newFile = e.target.files[0];
+    if (newFile) {
+      setFileList([newFile]);
+      props.onFileChange(newFile);
+    }
+  };
+
+  const fileRemove = (file) => {
+    const updatedList = [...fileList];
+    updatedList.splice(fileList.indexOf(file), 1);
+    setFileList(updatedList);
+    props.onFileChange(updatedList);
+  };
+
+  return (
+    <>
+      <div
+        ref={wrapperRef}
+        className="drop-file-input"
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}
+      >
+        <div className="drop-file-input__label">
+          <img src={uploadImg} alt="" />
+
+          <h5 className="mt-2">Select pdf,doc,docx,txt</h5>
+          <p>or drag and drop it here</p>
+        </div>
+        <input type="file" value="" onChange={onFileDrop} />
+      </div>
+      {fileList.length > 0 ? (
+        <div className="drop-file-preview">
+          <p className="drop-file-preview__title">Ready to upload</p>
+          {fileList.map((item, index) => (
+            <div key={index} className="drop-file-preview__item">
+              <img
+                src={
+                  ImageConfig[item.type.split("/")[1]] || ImageConfig["default"]
+                }
+                alt=""
+              />
+              <div className="drop-file-preview__item__info">
+                <p>
+                  {item.name} <br />
+                  {item.size}B
+                </p>
+              </div>
+              <span
+                className="drop-file-preview__item__del"
+                onClick={() => fileRemove(item)}
+              >
+                <FaTrashAlt className="del-file" />
+              </span>
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+DropFileInput.propTypes = {
+  onFileChange: PropTypes.func,
+};
+
+export default DropFileInput;
