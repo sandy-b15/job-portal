286dd3a1389aff23cd1b721de9695446deee43f2
arjun singh <arjun.singh@gmail.com>
2022-09-28 17:49:35 +0530

    removed links

--- src/components/IndividualJobs.js
+++ src/components/IndividualJobs.js
@@ -23,24 +23,22 @@ const IndividualJobs = (props) => {
     newString = trimmedString;
   }
   return (
-    <div className="col-lg-3 col-md-4 ">
-      <div className="card job_card">
-        <div className="card-body bodycard">
-          <h5 className="card-title mb-2">{props.title}</h5>
-          <h6 className="card-subtitle mb-4 ">
-            Experience:
-            {props.experience === null ? " Any" : ` ${props.experience}`}
-          </h6>
-          <h6 className="card-subtitle mb-3">Job Description</h6>
-          <div
-            className="description-div"
-            dangerouslySetInnerHTML={{ __html: props.description }}
-          ></div>
-          <div className="btn-actions">
-            <button className="apply-button mb-auto" onClick={onPress}>
-              Apply
-            </button>
-          </div>
+    <div className="card job_card">
+      <div className="card-body bodycard">
+        <h5 className="card-title mb-2">{props.title}</h5>
+        <h6 className="card-subtitle mb-4 ">
+          Experience:
+          {props.experience === null ? " Any" : ` ${props.experience}`}
+        </h6>
+        <h6 className="card-subtitle mb-3">Job Description</h6>
+        <div
+          className="description-div"
+          dangerouslySetInnerHTML={{ __html: props.description }}
+        ></div>
+        <div className="btn-actions">
+          <button className="apply-button mb-auto" onClick={onPress}>
+            Apply
+          </button>
         </div>
       </div>
     </div>
