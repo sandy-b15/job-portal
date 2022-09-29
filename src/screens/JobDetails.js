8abb975b9da0a07ea3a1ef74338908fac44bb22e
rupasree <rupasree124@gmail.com>
2022-09-29 18:34:32 +0530

    added container to JobDetails page

--- src/screens/JobDetails.js
+++ src/screens/JobDetails.js
@@ -50,63 +50,68 @@ function JobDetails(props) {
     myRef.current.scrollIntoView();
   };
 
+  console.log(Math.floor(Math.random()*100)+1)
+
   return (
     <>
       <LoadingScreen loading={loading}>
         <div className="job_details">
-          <div className="jobPage-logo">
-            <img src={logo} alt="Lucida's logo" />
-          </div>
-          <div className="info-card">
-            <div className="box">
-              <Link to="/" element={<JobOpenings />} className="nav">
-                <FaChevronLeft className="backarrow" />
-                &nbsp;Back
-              </Link>
-              <button className="applyJob-button" onClick={onClickHandler}>
-                Apply
-              </button>
+          <div className="container">
+            <div className="jobPage-logo">
+              <img src={logo} alt="Lucida's logo" />
             </div>
-            <div className="jobTitle" style={{ marginTop: 40 }}>
-              {content}
-              {jobDetails?.title && <h1>{jobDetails.title}</h1>}
-              {jobDetails?.location && <p>{jobDetails.location}</p>}
-            </div>
-            <div className="header-main">
-              <h2 className="title">Who We Are</h2>
-              <p>
-                'Lucida Technologies' is a Bangalore based Technology firm
-                specializing in the areas of Digital and Analytics
-                solutions,Machine Learning and Artificial Intelligence.We cater
-                to multiple clients in India,Malaysia,Singapore and USA spanning
-                across various domains and industries
-              </p>
-            </div>
-          </div>
-
-          <div
-            className="header"
-            dangerouslySetInnerHTML={{ __html: jobDetails?.description }}
-          ></div>
-
-          {
-            jobDetails?.skills.length !==0  && ( 
-              <div className="header">
-                <p className="title skills-para">Skills</p>
-                <ul>
-                  {jobDetails?.skills.map((skill) => (
-                    <li className="skills-list">{skill}</li>
-                  ))}
-                </ul>
+            <div className="info-card">
+              <div className="box">
+                <Link to="/" element={<JobOpenings />} className="nav">
+                  <FaChevronLeft className="backarrow" />
+                  &nbsp;Back
+                </Link>
+                <button className="applyJob-button" onClick={onClickHandler}>
+                  Apply
+                </button>
               </div>
-            )
-          }
+              <div className="jobTitle" style={{ marginTop: 40 }}>
+                {content}
+                {jobDetails?.title && <h1>{jobDetails.title}</h1>}
+                {jobDetails?.location && <p>{jobDetails.location}</p>}
+              </div>
+              <div className="header-main">
+                <h2 className="title">Who We Are</h2>
+                <p>
+                  'Lucida Technologies' is a Bangalore based Technology firm
+                  specializing in the areas of Digital and Analytics
+                  solutions,Machine Learning and Artificial Intelligence.We cater
+                  to multiple clients in India,Malaysia,Singapore and USA spanning
+                  across various domains and industries
+                </p>
+              </div>
+            </div>
 
+            <div
+              className="header"
+              dangerouslySetInnerHTML={{ __html: jobDetails?.description }}
+            ></div>
+
+            {
+              jobDetails?.skills.length !== 0 && (
+                <div className="header">
+                  <p className="title skills-para">Skills</p>
+                  <ul className="skills-list">
+                    {jobDetails?.skills.map((skill) => (
+                      <li key={Math.floor(Math.random()*100)+1}>{skill}</li>
+                    ))}
+                  </ul>
+                </div>
+              )
+            }
+
+          
           <JobForm
             jobId={jobDetails?.id}
             refProp={myRef}
             onLoading={loadingHandler}
           />
+        </div>
         </div>
         <Footer />
       </LoadingScreen>
